74f970da59ae20a6ec5ec7b79ba184c51235ff0e
Bosa7 <marko.bosnjak.bosa.93@gmail.com>
2020-11-22 13:53:02 +0100

    Update

--- client/src/views/ForgotCredentials.vue
+++ client/src/views/ForgotCredentials.vue
@@ -39,7 +39,7 @@
 <script>
     import "bootstrap";
     import "bootstrap/dist/css/bootstrap.min.css";
-	var axios = require("axios");
+    var axios = require("axios");
 	
     export default {
         name: "forgotCredentials",
